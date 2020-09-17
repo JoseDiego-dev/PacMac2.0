63761ce636152758dddcf06e2e942d28a78fe7c7
Emma Gomez <a00827097@itesm.mx>
2020-09-17 10:50:18 -0500

    Comentarios
    comentarios del código

--- PacMan2.py
+++ PacMan2.py
@@ -13,19 +13,20 @@ Exercises
 from random import choice
 from turtle import *
 from freegames import floor, vector
+#se importan las librerias de turtle y freegames
 
-state = {'score': 0}
+state = {'score': 0} #inicia un diccionario para llevar el puntaje
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
-aim = vector(5, 0)
-pacman = vector(-40, -80)
-ghosts = [
+aim = vector(5, 0) #vector que determina la direccion a la que se mueven los personajes
+pacman = vector(-40, -80) #vector del pacman
+ghosts = [ #vector de los cuatro fantasmas
     [vector(-180, 160), vector(5, 0)],
     [vector(-180, -160), vector(0, 5)],
     [vector(100, 160), vector(0, -5)],
     [vector(100, -160), vector(-5, 0)],
 ]
-tiles = [
+tiles = [ #vector del tablero
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
     0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
@@ -49,12 +50,14 @@ tiles = [
 ]
 
 def square(x, y):
+#funcion que define y dibuja el cuadrado del tablero
     "Draw square using path at (x, y)."
     path.up()
     path.goto(x, y)
     path.down()
     path.begin_fill()
 
+#limitaciones
     for count in range(4):
         path.forward(20)
         path.left(90)
@@ -62,6 +65,7 @@ def square(x, y):
     path.end_fill()
 
 def offset(point):
+# para contar el puntaje de los puntitos que come el pacman
     "Return offset of point in tiles."
     x = (floor(point.x, 20) + 200) / 20
     y = (180 - floor(point.y, 20)) / 20
@@ -69,6 +73,7 @@ def offset(point):
     return index
 
 def valid(point):
+#dice si el tile a la que se va a mover es valida
     "Return True if point is valid in tiles."
     index = offset(point)
 
@@ -83,13 +88,15 @@ def valid(point):
     return point.x % 20 == 0 or point.y % 20 == 0
 
 def world():
+#funcion que crea el tablero a partir del vector
     "Draw world using path."
-    bgcolor('black')
+    bgcolor('black') #colores
     path.color('blue')
 
     for index in range(len(tiles)):
         tile = tiles[index]
 
+    #dibuja los puntitos blancos del tablero
         if tile > 0:
             x = (index % 20) * 20 - 200
             y = 180 - (index // 20) * 20
@@ -101,12 +108,14 @@ def world():
                 path.dot(2, 'white')
 
 def move():
+#funcion del movimiento de los personajes
     "Move pacman and all ghosts."
     writer.undo()
     writer.write(state['score'])
 
     clear()
 
+#mueve el pacman segun la tecla presionada
     if valid(pacman + aim):
         pacman.move(aim)
 
@@ -123,10 +132,12 @@ def move():
     goto(pacman.x + 10, pacman.y + 10)
     dot(20, 'yellow')
 
+#movimiento de los fantasmas
     for point, course in ghosts:
         if valid(point + course):
             point.move(course)
         else:
+        #Los siguientes vectores determinan los movimientos permitidos de los fantasmas y su modificación los permite recorrer mayores distancias en el mismo tiempo 
             options = [
                 vector(10, 0),
                 vector(-10, 0),
@@ -143,13 +154,17 @@ def move():
 
     update()
 
+
     for point, course in ghosts:
         if abs(pacman - point) < 20:
             return
 
+#ontimer repite la función que se le de en el primer argumento
+# con la velocidad dada en milisegundos en el segundo argumento
     ontimer(move, 10)
 
 def change(x, y):
+#redirige al pacman cuando se dirige a un lugar invalido
     "Change pacman aim if valid."
     if valid(pacman + vector(x, y)):
         aim.x = x
@@ -162,6 +177,7 @@ writer.goto(160, 160)
 writer.color('white')
 writer.write(state['score'])
 listen()
+#vincula las flechas con el movimiento del pacman
 onkey(lambda: change(5, 0), 'Right')
 onkey(lambda: change(-5, 0), 'Left')
 onkey(lambda: change(0, 5), 'Up')
